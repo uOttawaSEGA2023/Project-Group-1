6a416b44354edd2ad1f7c6823109070e167c7a4d
vivekperbhoo <perbhoovivek@gmail.com>
2023-09-26 19:19:50 -0400

    Updated signup page

--- Appointwellapp copy/app/src/main/java/com/example/appointwellapp/MainActivity.java
+++ Appointwellapp copy/app/src/main/java/com/example/appointwellapp/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.appointwellapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioButton radioButtonPatient;
+    RadioButton radioButtonDoctor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        radioButtonPatient = findViewById(R.id.patientbtn);
+        radioButtonDoctor = findViewById(R.id.doctorbtn);
+        TextView healthcardno = findViewById(R.id.healthcardnumbertxt);
+        EditText specialtiesinput = findViewById(R.id.specialtiesinput);
+        TextView specialtiestext = findViewById(R.id.specialtiestxt);
+        LinearLayout fieldlayout = findViewById(R.id.fieldslayout);
+
+        radioButtonPatient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                healthcardno.setText("Health Card Number");
+                specialtiestext.setVisibility(View.GONE);
+                specialtiesinput.setVisibility(View.GONE);
+                specialtiesinput.setEnabled(false);
+            }
+        });
+
+        radioButtonDoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                healthcardno.setText("Employee Number");
+                specialtiestext.setVisibility(View.VISIBLE);
+                specialtiesinput.setVisibility(View.VISIBLE);
+                specialtiesinput.setEnabled(true);
+
+            }
+        });
     }
+
+
 }
