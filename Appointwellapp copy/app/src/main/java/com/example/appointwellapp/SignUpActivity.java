3d2338e1230cd41a4ce227c06d860e0fbd8720bc
tanyapyndiah <tppyndiah@me.com>
2023-10-05 12:18:52 -0400

    Added validation for most remaining fields

--- Appointwellapp copy/app/src/main/java/com/example/appointwellapp/SignUpActivity.java
+++ Appointwellapp copy/app/src/main/java/com/example/appointwellapp/SignUpActivity.java
@@ -3,6 +3,8 @@ package com.example.appointwellapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -10,6 +12,8 @@ import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -87,33 +91,113 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
-        emailInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+
+        phoneNumberInput.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onFocusChange(View view, boolean b) {
-                if (!(view.hasFocus())){
-                    // call email validate function
-                }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                NumberIsValid(phoneNumberInput);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
 
-        firstNameInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        emailInput.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                nameIsValid(v, firstNameInput);
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                emailIsValid(emailInput);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
 
-        lastNameInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        healthCardNumberInput.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                nameIsValid(v, lastNameInput);
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                NumberIsValid(healthCardNumberInput);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        passwordInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                passwordIsValid(passwordInput);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        firstNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                nameIsValid(firstNameInput);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        lastNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                nameIsValid(lastNameInput);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
-    private void nameIsValid(View v, EditText input){
-        if (!(v.hasFocus())) {
+    private boolean nameIsValid(EditText input){
+        boolean valid = true;
+
             String firstName = String.valueOf(input.getText());
-            boolean valid = true;
             if (firstName.matches("")) {
                 input.setError("Field cannot be empty.");
                 valid = false;
@@ -128,12 +212,111 @@ public class SignUpActivity extends AppCompatActivity {
                     }
                 }
             }
-            if (valid) {
-                input.setError(null);
-            }
-        }
+
+
+        return valid;
     }
 
+    private boolean NumberIsValid(EditText input ){
+        boolean valid= true;
+
+
+
+            String phoneNum= String.valueOf(input.getText());
+            if (phoneNum.length()!= 10){
+                valid=false;
+                if (phoneNum.length()==0){
+                    input.setError("Field cannot be empty.");
+                } else {
+                    input.setError("Number should consist of 10 numbers.");
+                }
+            } else {
+                for (int i=0; i<10; i++){
+                    if (phoneNum.charAt(i)<48 || phoneNum.charAt(i)>57){
+                        valid=false;
+                        input.setError("Number can only consist of numerical characters.");
+                        break;
+                    }
+                }
+            }
+
+
+
+        return valid;
+    }
+
+    private boolean emailIsValid(EditText input){
+        boolean valid;
+        String regex="^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+            String email= String.valueOf( input.getText());
+            valid= (Pattern.compile(regex).matcher(email).matches());
+            if (!valid){
+                input.setError("Email address format is incorrect.");
+            }
+
+        return valid;
+    }
+    private boolean passwordIsValid(EditText input){
+            boolean valid=true;
+            char c;
+            boolean specialChar=false;
+            boolean uppercase=false;
+            boolean lowercase=false;
+            boolean num=false;
+
+            String password= String.valueOf(input.getText());
+            if (password.length()<9){
+                valid=false;
+                input.setError("Password consist of at least 8 characters.");
+            } else {
+                for (int i=0; i<password.length();i++){
+                    c=password.charAt(i);
+                    if (c>= 65 && c<=90){
+                        uppercase=true;
+                    } else {
+                        if (c>=97 && c<=122){
+                            lowercase=true;
+                        } else {
+                            if (c>=48 && c<=57){
+                                num=true;
+                            } else {
+                                if ((c>=33 && c<=47)|| (c>=58 && c<= 64)|| (c>=91 && c<=96)|| (c>=123 && c<=126)){
+                                    specialChar=true;
+                                } else {
+                                    valid = false;
+                                    input.setError("Password can only consist of numbers, uppercase or lowercase characters and symbols (~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/)");
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                }
+                if (!(uppercase && lowercase && specialChar && num)){
+                    valid=false;
+                    String errorMessage="";
+                    if (!uppercase){
+                        errorMessage=errorMessage+"Password should contain an uppercase alphabet. ";
+                    }
+                    if (!lowercase){
+                        errorMessage=errorMessage+"Password should contain a lowercase alphabet. ";
+                    }
+                    if (!num){
+                        errorMessage=errorMessage+"Password should contain a number. ";
+                    }
+                    if (!specialChar){
+                        errorMessage=errorMessage+"Password should contain a symbol.";
+                    }
+                    input.setError(errorMessage);
+                }
+            }
+
+
+
+
+        return valid;
+    }
 
 
 
