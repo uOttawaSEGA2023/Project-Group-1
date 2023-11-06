cfd9e2359c7aafd244554cafea559540339626af
Karen <113635618+KarenCKL@users.noreply.github.com>
2023-11-05 23:29:59 -0500

    Create Shift.java

---
+++ AppointWell/app/src/main/java/com/example/myapplication/Shift.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import android.os.Build;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+
+public class Shift {
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private LocalDate selectedDate;
+    private LocalDate CurrentDate = null;
+    public Shift() {
+    }
+    public void setStartTime(LocalTime startTime) {
+            this.startTime = startTime;
+    }
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+    public void setEndTime(LocalTime endTime) {
+            this.endTime= endTime ;
+    }
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setSelectedDate(LocalDate selectedDate) {
+        this.selectedDate =  selectedDate;
+    }
+
+    public LocalDate getSelectedDate() {
+        return selectedDate;
+    }
+
+    public LocalDate getCurrentDate() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            CurrentDate = LocalDate.now();
+        }
+        return CurrentDate;
+    }
+}
+
+
+
