5531738c3fbfe704a140ae2316fd2bb7b1b4da32
Karen <113635618+KarenCKL@users.noreply.github.com>
2023-11-10 07:48:24 -0500

    Update Doctor.java

--- AppointWell/app/src/main/java/com/example/myapplication/Doctor.java
+++ AppointWell/app/src/main/java/com/example/myapplication/Doctor.java
@@ -9,11 +9,10 @@ public class Doctor extends UserAccount{
 
 
     public Doctor(){}
-    public Doctor(String email, String password, String firstName, String lastName, String address, long employeeNumber, long phoneNumber, List<String> specialties, List<String>shifts) {
+    public Doctor(String email, String password, String firstName, String lastName, String address, long employeeNumber, long phoneNumber, List<String> specialties) {
         super(email, password, "Doctor", "Pending", firstName, lastName, address, phoneNumber);
         this.employeeNumber = employeeNumber;
         this.specialties = specialties;
-        this.shifts=shifts;
     }
 
     public List<String> getShifts() {
